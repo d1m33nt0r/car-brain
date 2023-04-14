6310d7d6f58ab7bf85d77fe5ae3cf795fd9fdce2
dmytrohryb <dmitrii.9rib@gmail.com>
2023-04-14 13:55:10 +0300

    Added GameBuilder script

---
+++ Assets/Editor/GameBuilder.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Editor
+{
+    public class GameBuilder : MonoBehaviour
+    {
+        [MenuItem("Build/Build macOS")]
+        public static void PerformMacOSBuild()
+        {
+            var buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scenes/Game.unity" };
+            buildPlayerOptions.locationPathName = "build/macOS/BirdBrain.app";
+            buildPlayerOptions.target = BuildTarget.StandaloneOSX;
+            buildPlayerOptions.options = BuildOptions.None;
+
+            var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            var summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + "bytes");
+            }
+            
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
+        }
+    }
+}
