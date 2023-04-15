2563f177d330fcff19e491cb62015ec5aa5d9b4b
dmytrohryb <dmitrii.9rib@gmail.com>
2023-04-15 23:47:08 +0300

    no message

--- Assets/Editor/GameBuilder.cs
+++ Assets/Editor/GameBuilder.cs
@@ -7,7 +7,29 @@ namespace Editor
 {
     public class GameBuilder : MonoBehaviour
     {
+        [MenuItem("Build/Build macOS")]
+        public static void PerformMacOSBuild()
+        {
+            var buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scenes/Game.unity" };
+            buildPlayerOptions.locationPathName = "build/macOS/BirdBrain.app";
+            buildPlayerOptions.target = BuildTarget.StandaloneOSX;
+            buildPlayerOptions.options = BuildOptions.None;
 
+            var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            var summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + "bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
+        }
+        
         [MenuItem("Build/Build iOS")]
         public static void PerformIOSBuild()
         {
